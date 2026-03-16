4fd0ea976d4a40651cb19c01419bfbc5b8d02bba
신정우 <inconcluso@dankook.ac.kr>
2026-03-16 15:04:48 +0900

    a

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# 패키지 설치
 pip install -r requirements.txt
 
-# 정적 파일 모으기 (나중에 관리자 페이지를 위해 필요)
-python manage.py collectstatic --no-input
-
-# 👇 [핵심] DB 마이그레이션 실행
+# DB 마이그레이션 먼저! (이게 성공해야 앱이 돌아갑니다)
 python manage.py migrate
+
+# 정적 파일 모으기 (에러 방지를 위해 --no-input 추가)
+python manage.py collectstatic --no-input
